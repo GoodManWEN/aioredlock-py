7cd3669955e0f332fbb5914c63bbf3ceffff7c29
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2022-04-16 19:33:05 +0100

    Fix: lua script to number

--- aioredlock_py/redisson.py
+++ aioredlock_py/redisson.py
@@ -38,7 +38,7 @@ class Redisson:
         self._sleepattr_k = (sleeptime_h - sleeptime_l)
         self._sleepattr_b = sleeptime_l
         self._sleepattr_avg = (sleeptime_h + sleeptime_l) / 2
-        self._get_lock = False
+        self._lock_acquired = False
         self._retry_times = retry_times
         self._daemon_extend_interval = ex * 0.667
         self._close_triggered = False
@@ -46,13 +46,13 @@ class Redisson:
         self.carry = carry
 
     def is_locked(self):
-        return self._get_lock
+        return self._lock_acquired
 
     async def __aenter__(self) -> Optional['Redisson']:
         for _ in range(self._retry_times): # CAS
             lock_status = await self.redis.set(self._lock_key, self._lock_uuid, nx=True, ex=self._ex)
             if lock_status:
-                self._get_lock = True
+                self._lock_acquired = True
                 loop = asyncio.get_running_loop()
                 self._daemon_task = loop.create_task(self._daemon_thread())
                 break
@@ -77,17 +77,17 @@ class Redisson:
         self._close_triggered = True
         if self._daemon_task:
             self._daemon_task.cancel()
-        if self._get_lock:
+        if self._lock_acquired:
             script0 = self._scriper[0]
             if not script0:
             	# By default, the first parameter is read using the `KEYS` keyword 
             	# and the second parameter is read using `ARGV` keyword. Here we pass 
             	# a tuple of length 2 to `KEYS`. Use the singleton pattern to avoid 
             	# repeated creation.
-                script0 = self.redis.register_script("if redis.call('get', KEYS[1]) == KEYS[2] then return redis.call('del', KEYS[1]) else return 0 end")
+                script0 = self.redis.register_script("if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end")
                 self._scriper[0] = script0
-            await script0((self._lock_key, self._lock_uuid))
-            self._get_lock = False
+            await script0(keys = (self._lock_key, ), args = (self._lock_uuid, ))
+            self._lock_acquired = False
         return False
 
     async def _daemon_thread(self):
@@ -95,15 +95,15 @@ class Redisson:
         # user's service logic takes longer than the default lock 
         # release time to avoid being occupied by other threads.
         while True:
-            await asyncio.sleep(self.self._daemon_extend_interval)
+            await asyncio.sleep(self._daemon_extend_interval)
             if self._close_triggered:
                 # To deal with special cases where lock release has not 
                 # occurred, ensure that the thread will certainly end 
                 # when the context manager is released.
                 break
-            if self._get_lock:
+            if self._lock_acquired:
                 script1 = self._scriper[1]
                 if not script1:
-                    script1 = self.redis.register_script("if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('expire', KEYS[1], ARGV[2]) else return 0 end")
+                    script1 = self.redis.register_script("if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('expire', KEYS[1], tonumber(ARGV[2])) else return 0 end")
                     self._scriper[1] = script1
-                await script1((self._lock_key, ), (self._lock_uid, self._ex))
+                await script1(keys = (self._lock_key, ), args = (self._lock_uuid, self._ex))
